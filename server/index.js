06b776fa0efcc35b29f2bc2320f40ae1ecc11ebc
jakehurrell <hakejurrell@gmail.com>
2019-06-26 20:20:55 -0400

    committing-pre heroku deployment

---
+++ server/index.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const app = express();
+
+app.get('/', (req, res) => {
+  res.send({ hi: 'there' });
+});
+
+const PORT = process.env.PORT || 5000;
+console.log(process.env);
+
+app.listen(PORT);
