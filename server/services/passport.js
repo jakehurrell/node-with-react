d21b01dc3f183accbf4173d4025efca61917c3c9
jakehurrell <hakejurrell@gmail.com>
2019-06-28 20:58:11 -0400

    google oauth implemented. raw implementation

--- server/services/passport.js
+++ server/services/passport.js
@@ -1,7 +1,19 @@
 const passport = require('passport');
 const GoogleStrategy = require('passport-google-oauth20').Strategy;
-
+const mongoose = require('mongoose');
 const keys = require('../config/keys');
+
+const User = mongoose.model('users');
+
+passport.serializeUser((user, done) => {
+  done(null, user.id);
+});
+
+passport.deserializeUser((id, done) => {
+  User.findById(id).then(user => {
+    done(null, user);
+  });
+});
 
 passport.use(
   new GoogleStrategy(
@@ -11,9 +23,15 @@ passport.use(
       callbackURL: '/auth/google/callback'
     },
     (accessToken, refreshToken, profile, done) => {
-      console.log('access token', accessToken);
-      console.log('refresh token', refreshToken);
-      console.log('profile', profile);
+      User.findOne({ googleId: profile.id }).then((err, existingUser) => {
+        if (existingUser) {
+          done(err, existingUser);
+        } else {
+          new User({ googleId: profile.id })
+            .save()
+            .then(user => done(null, user));
+        }
+      });
     }
   )
 );
