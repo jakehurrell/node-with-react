9c171db8b9c0838c29f27ddfa667a9919d3510f2
jakehurrell <hakejurrell@gmail.com>
2019-07-06 10:35:00 -0400

    credits with test payment implemented

--- server/client/src/components/Payments.js
+++ server/client/src/components/Payments.js
@@ -1,17 +1,26 @@
 import React from 'react';
 import StripeCheckout from 'react-stripe-checkout';
+import { connect } from 'react-redux';
+import * as actions from '../actions';
 
 class Payments extends React.Component {
   render() {
     return (
       <StripeCheckout
+        name='Emaily'
+        description='$5 for 5 email credits'
         amount={500}
-        token={token => console.log(token)}
+        token={token => this.props.handleToken(token)}
         stripeKey={process.env.REACT_APP_STRIPE_KEY}
         currency='CAD'
-      />
+      >
+        <button className='btn light-blue'>Add Credits</button>
+      </StripeCheckout>
     );
   }
 }
 
-export default Payments;
+export default connect(
+  null,
+  actions
+)(Payments);
