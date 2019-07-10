5d9555a986854148898c5dab8e84ba5fbb2f9f31
jakehurrell <hakejurrell@gmail.com>
2019-07-09 20:42:26 -0400

    beginning survey creation

--- server/index.js
+++ server/index.js
@@ -5,6 +5,7 @@ const passport = require('passport');
 const bodyParser = require('body-parser');
 const keys = require('./config/keys');
 require('./models/User');
+require('./models/Survey');
 require('./services/passport');
 
 mongoose.connect(keys.mongoURI);
@@ -19,12 +20,12 @@ app.use(
   })
 );
 
-//passport init
 app.use(passport.initialize());
 app.use(passport.session());
 
 require('./routes/authRoutes')(app);
 require('./routes/billingRoutes')(app);
+require('./routes/surveyRoutes')(app);
 
 if (process.env.NODE_ENV === 'production') {
   //serve production assets like main.js or main.css
--- server/routes/billingRoutes.js
+++ server/routes/billingRoutes.js
@@ -5,10 +5,6 @@ const requireLogin = require('../middlewares/requireLogin');
 
 module.exports = app => {
   app.post('/api/stripe', requireLogin, async (req, res) => {
-    if (!req.user) {
-      return res.status(401).send({ Error: 'You must log in' });
-    }
-
     const charge = await stripe.charges.create({
       amount: 500,
       currency: 'CAD',
