8f511b3e871c2e7613a93e097a35d721e53531c3
jakehurrell <hakejurrell@gmail.com>
2019-07-16 20:27:33 -0400

    new survey UI beginning

---
+++ server/client/src/components/surveys/SurveyForm.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import { reduxForm, Field } from 'redux-form';
+
+class SurveyForm extends React.Component {
+  render() {
+    return (
+      <div>
+        <form onSubmit={this.props.handleSubmit(values => console.log(values))}>
+          <Field type="text" name="surveyTitle" component="input" />
+        </form>
+      </div>
+    );
+  }
+}
+
+export default reduxForm({
+  form: 'surveyForm'
+})(SurveyForm);
